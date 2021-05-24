13758096129d829a0d8018b3b16ef2bb59c07491
LibenHailu <libenhailu04@gmail.com>
2021-05-24 16:12:17 +0300

    fix added

--- src/app/application.go
+++ src/app/application.go
@@ -2,10 +2,10 @@ package app
 
 import (
 	"github.com/LibenHailu/bookstore_oauth_api/src/http"
-	"github.com/gin-gonic/gin"
-
-	"github.com/LibenHailu/bookstore_oauth_api/src/domain/access_token"
 	"github.com/LibenHailu/bookstore_oauth_api/src/repository/db"
+	"github.com/LibenHailu/bookstore_oauth_api/src/repository/rest"
+	"github.com/LibenHailu/bookstore_oauth_api/src/services/access_token"
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -13,10 +13,10 @@ var (
 )
 
 func StartApplication() {
-	atService := access_token.NewService(db.NewRepository())
-	atHanler := http.NewHandler(atService)
-	router.GET("/oauth/access_token/:access_token_id", atHanler.GetById)
-	router.POST("/oauth/access_token", atHanler.Create)
+	atHandler := http.NewHandler(access_token.NewService(rest.NewRepository(), db.NewRepository()))
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+	router.POST("/oauth/access_token", atHandler.Create)
 
 	router.Run(":8080")
 }
--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,14 +1,18 @@
 package access_token
 
 import (
+	"fmt"
 	"strings"
 	"time"
 
+	"github.com/LibenHailu/bookstore_oauth_api/src/utils/crypto_utils"
 	"github.com/LibenHailu/bookstore_oauth_api/src/utils/errors"
 )
 
 const (
-	expirationTime = 24
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grantTypeClientCredentials = "client_credentials"
 )
 
 type AccessToken struct {
@@ -16,6 +20,33 @@ type AccessToken struct {
 	UserId      int64  `json:"user_id"`
 	ClientId    int64  `json:"client_id"`
 	Expires     int64  `json:"expires"`
+}
+
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+	// Used for password grant type
+	Username string `json:"username"`
+	Password string `json:"password"`
+	// Used for client_credentials grant type
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (at *AccessTokenRequest) Validate() *errors.RestErr {
+	switch at.GrantType {
+	case grantTypePassword:
+		break
+
+	case grantTypeClientCredentials:
+		break
+	default:
+		return errors.NewBadRequestError("invalid grant_type parameter")
+	}
+	// if at.GrantType != grantTypePassword || at.GrantType != grantTypeClientCredentials {
+	// 	return errors.NewBadRequestError("invalid grant_type parameter")
+	// }
+	return nil
 }
 
 func (at *AccessToken) Validate() *errors.RestErr {
@@ -35,12 +66,17 @@ func (at *AccessToken) Validate() *errors.RestErr {
 	return nil
 }
 
-func GetNewAccessToken() AccessToken {
+func GetNewAccessToken(userId int64) AccessToken {
 	return AccessToken{
+		UserId:  userId,
 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
 
 func (at *AccessToken) IsExpired() bool {
 	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
+}
+
+func (at *AccessToken) Generate() {
+	at.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
